243a613cb3c962ab1f03c785788556fd82c78018
abbeyshubh <shubhamabbey54@gmail.com>
2020-07-11 23:43:12 +0530

    build

---
+++ build/precache-manifest.32edeb98433d7ca61394448058645a0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0a8996a303081180bfe0572bf2363b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7954d90a72cf1ec10a90",
+    "url": "/static/css/2.e3a86d8a.chunk.css"
+  },
+  {
+    "revision": "f52f668bacc76b2ebfbf",
+    "url": "/static/css/main.266b60a8.chunk.css"
+  },
+  {
+    "revision": "7954d90a72cf1ec10a90",
+    "url": "/static/js/2.bbabc55a.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.bbabc55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52f668bacc76b2ebfbf",
+    "url": "/static/js/main.94ed7075.chunk.js"
+  },
+  {
+    "revision": "cfad997bec767bfa3a9a",
+    "url": "/static/js/runtime-main.2bbc8c70.js"
+  }
+]);
